4213eba193328008c0bbb629c85500a6c4ceb0cf
JP <jpyepezimc@gmail.com>
2019-07-10 10:29:16 +1200

    added moviesapp

---
+++ MoviesApp/precache-manifest.e52c292ccffcb4ff9bb918a454c19d47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc2619925ca663f992f786c6911099c",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "9bce2e859de62c61cb58",
+    "url": "/MoviesApp/static/css/main.3da4600d.chunk.css"
+  },
+  {
+    "revision": "1887fe31499d560adbe5",
+    "url": "/MoviesApp/static/js/2.6a194b1f.chunk.js"
+  },
+  {
+    "revision": "9bce2e859de62c61cb58",
+    "url": "/MoviesApp/static/js/main.740b09ac.chunk.js"
+  },
+  {
+    "revision": "cac95804670d72d2c5b9",
+    "url": "/MoviesApp/static/js/runtime~main.d18671c3.js"
+  }
+]);
